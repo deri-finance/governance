8f2999d619c51a01051ca8f2fcfbb252f43ebc70
黎海冬 <haidong.li@dfactory.tech>
2021-06-28 19:45:30 +0800

    release governance

---
+++ static/js/30.885f13c4.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpapp.deri.finance"]=this["webpackJsonpapp.deri.finance"]||[]).push([[30],{720:function(n,a,t){},741:function(n,a,t){"use strict";t.r(a),t.d(a,"default",(function(){return c}));var e=t(666),i=(t(720),t(56));function c(n){var a=n.lang;return Object(i.jsx)(e.a,{lang:a})}}}]);
+//# sourceMappingURL=30.885f13c4.chunk.js.map
